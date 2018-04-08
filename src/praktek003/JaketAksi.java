ba52978eca2ecc594b572be78bf902ac40d5602d
Habib06 <habib06@gmail.com>
2018-04-08 13:36:12 +0800

    instance, buat atribut dan panggil method

--- src/praktek003/JaketAksi.java
+++ src/praktek003/JaketAksi.java
@@ -1,6 +1,11 @@
 package praktek003;
 public class JaketAksi {
     public static void main(String[] args) {
+        Jaket s = new Jaket();
+        s.merk="Volcom";
+        s.warna="Hitam";
+        s.harga=100000;
         
+        s.cetakInfo();
     }
 }
