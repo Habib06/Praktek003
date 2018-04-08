48c5de355b2598204da00996b1b003d15a096c7a
Habib06 <habib06@gmail.com>
2018-04-08 13:48:50 +0800

    menggunakan method set

--- src/praktek003/JaketAksi.java
+++ src/praktek003/JaketAksi.java
@@ -2,9 +2,10 @@ package praktek003;
 public class JaketAksi {
     public static void main(String[] args) {
         Jaket s = new Jaket();
-        s.merk="Volcom";
-        s.warna="Hitam";
-        s.harga=100000;
+       
+        s.setMerk("Volcom");
+        s.setWarna("Hitam");
+        s.setHarga(100000);
         
         s.cetakInfo();
     }
